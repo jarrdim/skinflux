e3dd04d9d4665ada940b93716de4a6179755d7a6
jack <85497086+jarrdim@users.noreply.github.com>
2024-08-08 19:39:48 +0300

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -32,7 +32,7 @@ export default async function Home({ searchParams }: Props) {
         </div>
         <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap:8">
           {products.map((product: any) => {
-            return <ProductCard data={product} />;
+            return <ProductCard key={product.id} data={product} />;
           })}
         </div>
       </div>
