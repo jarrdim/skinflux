d3fab8b8962dbf53cf8a2edc57dbf94f7b117cfe
jack <85497086+jarrdim@users.noreply.github.com>
2024-08-09 02:36:57 +0300

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images:{
-        domains:['m.media-amazon.com','lh3.googleusercontent.com','fiverr-res.cloudinary.com','www.dropbox.com'],
+        domains:['dl.dropboxusercontent.com','m.media-amazon.com','lh3.googleusercontent.com','fiverr-res.cloudinary.com','www.dropbox.com'],
     },
 };
 
