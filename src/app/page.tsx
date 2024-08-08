2c1b3266556e8a9a03366ead167cdcc5a209e74b
jack <85497086+jarrdim@users.noreply.github.com>
2024-08-09 01:01:36 +0300

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,36 @@
-import { GetServerSideProps } from 'next';
 import Banner from "@/components/Banner";
+import Container from "@/components/Container";
+import ProductCard from "@/components/products/ProductCard";
+import SearchBar from "@/components/SearchBar";
+import getProducts, { ProductParams } from "../actions/getProducts";
 
-
-export default function Home() {
-
-
-  return (
-
-      <div className="p-8">
-        <Banner />
-      
-        kkkkkkkkkkkkkkkkkkkk
-        </div>
-     
-    
-  );
+interface Props {
+  searchParams: ProductParams;
 }
 
+export default async function Home({ searchParams }: Props) {
+  // Fetch the products before returning JSX
+  const products = await getProducts(searchParams);
 
+  // Static rendering logic is separated from the dynamic fetch
+  if (products.length === 0) {
+    return (
+      <div className="text-center justify-center p-6">No product found</div>
+    );
+  }
+
+  // Render the component with the fetched products
+  return (
+    <Container>
+      <div className="p-8">
+        <Banner />
+        <SearchBar />
+        <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-8">
+          {products.map((product) => (
+            <ProductCard key={product.id} data={product} />
+          ))}
+        </div>
+      </div>
+    </Container>
+  );
+}
