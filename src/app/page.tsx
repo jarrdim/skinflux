6f2368572bad4f791d0c32ee1ba419953ffcfdc9
jack <85497086+jarrdim@users.noreply.github.com>
2024-08-09 00:03:39 +0300

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,46 +1,43 @@
-import Image from "next/image";
+import { GetServerSideProps } from 'next';
 import Banner from "@/components/Banner";
 import Container from "@/components/Container";
-  import { products } from "../../utils/products";
-import truncate from "../../utils/truncate";
 import ProductCard from "@/components/products/ProductCard";
 import SearchBar from "@/components/SearchBar";
-//import getProducts, { ProductParams } from "../actions/getProducts";
+import getProducts, { ProductParams } from "../actions/getProducts";
 
 interface Props {
-  searchParams: ProductParams;
+  products: any[];
 }
-export default async function Home({ searchParams = {} }: Props) {
 
-  //FROM DB
-
- // const products = await getProducts(searchParams);
-  // const products = await getProducts(searchParams).catch(error => {
-  //     console.error("Failed to fetch products:", error);
-  //     return []; // Return an empty array or handle as needed
-  //   });
-
-  if(products.length === 0)
-  {
-    return <div className="text-center justify-center p-6">No product found</div>
+export default function Home({ products }: Props) {
+  if (products.length === 0) {
+    return (
+      <div className="text-center justify-center p-6">No product found</div>
+    );
   }
 
-  //
   return (
     <Container>
-      <div className="p-8 ">
-        <div>
-          <Banner />
-        </div>
-        <div>
-          <SearchBar />
-        </div>
-        <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap:8">
-          {products.map((product: any) => {
-            return <ProductCard key={product.id} data={product} />;
-          })}
+      <div className="p-8">
+        <Banner />
+        <SearchBar />
+        <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-8">
+          {products.map((product) => (
+            <ProductCard key={product.id} data={product} />
+          ))}
         </div>
       </div>
     </Container>
   );
 }
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const searchParams = context.query as ProductParams;
+  const products = await getProducts(searchParams);
+
+  return {
+    props: {
+      products,
+    },
+  };
+};
