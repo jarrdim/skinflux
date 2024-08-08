913441bf1cba5f0ac997da4d22862dfa3f5f747f
jack <85497086+jarrdim@users.noreply.github.com>
2024-08-09 00:42:49 +0300

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,19 @@
 import { GetServerSideProps } from 'next';
 import Banner from "@/components/Banner";
-import Container from "@/components/Container";
 
 
 export default function Home() {
 
 
   return (
-    <Container>
+
       <div className="p-8">
         <Banner />
       
         kkkkkkkkkkkkkkkkkkkk
         </div>
       </div>
-    </Container>
+    
   );
 }
 
