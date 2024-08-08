9426280d9df380a2f0aa37fa732b830faf929626
jack <85497086+jarrdim@users.noreply.github.com>
2024-08-09 00:12:25 +0300

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,43 +1,21 @@
 import { GetServerSideProps } from 'next';
 import Banner from "@/components/Banner";
 import Container from "@/components/Container";
-import ProductCard from "@/components/products/ProductCard";
-import SearchBar from "@/components/SearchBar";
-import getProducts, { ProductParams } from "../actions/getProducts";
 
-interface Props {
-  products: any[];
-}
 
-export default function Home({ products }: Props) {
-  if (products.length === 0) {
-    return (
-      <div className="text-center justify-center p-6">No product found</div>
-    );
-  }
+export default function Home() {
+
 
   return (
     <Container>
       <div className="p-8">
         <Banner />
         <SearchBar />
-        <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-8">
-          {products.map((product) => (
-            <ProductCard key={product.id} data={product} />
-          ))}
+        kkkkkkkkkkkkkkkkkkkk
         </div>
       </div>
     </Container>
   );
 }
 
-export const getServerSideProps: GetServerSideProps = async (context) => {
-  const searchParams = context.query as ProductParams;
-  const products = await getProducts(searchParams);
 
-  return {
-    props: {
-      products,
-    },
-  };
-};
